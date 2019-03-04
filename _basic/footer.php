2bffbf0f7bb58b90fbf3bc4306d29a80c649da7d
Vladislav Fedorov <contact@vladislavdrews.com>
2019-03-04 11:56:39 +0300

    Second commit

---
+++ _basic/footer.php
@@ -0,0 +1,9 @@
+<!-- FOOTER -->
+
+<footer class="page-footer font-small bd-menu">
+
+	<div class="footer-copyright text-center py-3" > <?php echo date(Y)?> Copyright:
+		<a href="http://vladislavdrews.com"> vladislavdrews.com</a>
+	</div>
+
+</footer>
