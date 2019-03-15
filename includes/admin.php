f08b885b0a6857afcc85ac619d412276c3fd35c9
Vladislav Fedorov <contact@vladislavdrews.com>
2019-03-15 17:21:39 +0300

    Delete some code

--- includes/admin.php
+++ includes/admin.php
@@ -37,52 +37,7 @@ $db = mysqli_connect($hostname, $username, $password, $database) or die ("ERROR:
 mysqli_select_db($db, $database) or die ("ERROR: DB");
 
 
-session_start();
-
-if(isset($_SESSION["session_username"])){
-	// вывод "Session is set"; // в целях проверки
-	header("Location: intropage.php");
-}
-
-if(isset($_POST["login"])){
-	
-if(!empty($_POST['username']) && !empty($_POST['password'])) {
-	$username=htmlspecialchars($_POST['username']);
-	$password=htmlspecialchars($_POST['password']);
-	$query =mysql_query("SELECT * FROM usertbl WHEREusername='".$username."' AND password='".$password."'");
-	$numrows=mysql_num_rows($query);
-	if($numrows!=0)
- {
-	 
-while($row = mysql_fetch_assoc($query)){
-		$dbusername = $row['username'];
-		$dbpassword = $row['password'];
-}
-  if($username == $dbusername && $password == $dbpassword)
- {
-	// старое место расположения
-	//  session_start();
-	 $_SESSION['session_username']=$username;	 
- /* Перенаправление браузера */
-   header("Location: intropage.php");
-	}
-	} else {
-	//  $message = "Invalid username or password!";
-	
-	echo  "Invalid username or password!";
- }
-
-} else {
-    $message = "All fields are required!";
-	}
-	}
-
-
-
 ?>
-
-
-
 
 
 
