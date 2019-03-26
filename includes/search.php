a0bb5b29ee4ca170555612f41db7cd44c8a21c90
Vladislav Fedorov <contact@vladislavdrews.com>
2019-03-26 18:19:11 +0300

    Start new scripts for the search service

--- includes/search.php
+++ includes/search.php
@@ -8,16 +8,16 @@ $collectiontable = "collection";
 $wantedtable = "wantedlist";
 
 $search_q = $_POST['search_q'];
-$l= mysqli_connect($hostname, $username, $password, $database) or die ("ERROR: MySQL.");
+$l = mysqli_connect($hostname, $username, $password, $database) or die ("ERROR: MySQL.");
 
 $search_q = trim($search_q);
 $search_q = strip_tags($search_q);
 
-$q= mysqli_query($l, "SELECT `collection_band` FROM `$collectiontable` WHERE `collection_band` LIKE '%$search_q%'");
-$itog=mysqli_fetch_assoc($q);
+$q = mysqli_query($l, "SELECT `collection_band` FROM `$collectiontable` WHERE `collection_band` LIKE '%$search_q%'");
+$final = mysqli_fetch_assoc($q);
 
- while ($itog = mysqli_fetch_assoc($q)) {
-	printf("%s (%s)\n",$r["title_value"],$r["content"]);
+ while ($final = mysqli_fetch_assoc($q)) {
+	printf("%s (%s)\n",$r["collection_band"]);
  };
  
  mysqli_free_result($q);
