cee517aff9aeb2c14fa703a35fbf03033f5fb716
Vladislav Fedorov <contact@vladislavdrews.com>
2019-03-29 11:00:58 +0300

    Small things fixed

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 
+<html lang = "en"> 
 <head>
 	<meta charset="utf-8">
 	<title>Vinyl collection</title>
@@ -129,3 +130,4 @@ echo '</div>';
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </body>
+</html>
