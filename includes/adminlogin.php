9e07048fb78d037248c3037b90e6ed3184e0bdda
Vladislav Fedorov <contact@vladislavdrews.com>
2019-03-29 13:12:53 +0300

    Added a valid forms inputs check (JavaScript) on the adminlogin page)

--- includes/adminlogin.php
+++ includes/adminlogin.php
@@ -1,95 +1,69 @@
 <!DOCTYPE html>
 
+<html lang = "en"> 
 <head>
 	<meta charset="utf-8">
-	<title>Administration</title>
+	<title>Admin</title>
+	
+	<!-- For Bootstrap -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	
 	<style>
-
+	h1, .centered { text-align: center; }
+	.own-style {
+		width: 200px;
+		margin: auto;
+		margin-bottom: 10px;
+		text-align: center;
+	}
 	</style>
 </head>
 
 <body>
 
-<!--
+<h1 class="h3 mb-3 font-weight-normal">: Authorisation :</h1>
 
-<form action="" method="post">
-	<p><input type="email" id="input_email" placeholder="Login" name="username"></p>
-	<p><input type="password" id="input_password" placeholder="Password" name="password"></p>
-	<p><a href="adminlogin.php" type="submit">Login</a></p>
+<div class="centered">
+<form name="contact_form" method="post" onsubmit="return validate()" class="form-signin">
+	<input type="text" name="email" class="form-control own-style"  value="Your Email">
+	<input type="text" name="password" class="form-control own-style" value="Your Password">
+	<input type="submit" value="Log In" class="btn btn-primary own-style">
 </form>
+</div>
 
--->
+<script>
 
-<form action="index.php" method="post">
-    <label for="login"><span class="indicators">Логин: </span></label>
-    <input type="text" id="login" name="login" class="inputs">
-	
-    <label for="password"><span class="indicators">Пароль: </span></label>
-    <input type="password" id="password" name="password" class="inputs">
-	
-    <input type="submit" value="Enter" id="signinbutton">
-</form>
+function validate(){
+   
+   var email = document.forms["contact_form"]["email"].value;
+   var password = document.forms["contact_form"]["password"].value;
 
+   if (email.length == 0){
+      alert("Please enter your email");
+      return false;
+   }
 
-<?php
+   if (password.length == 0){
+      alert("Please enter your password.");
+      return false;
+   }
 
+   var at = email.indexOf("@");
+   var dot = email.indexOf(".");
 
-## Information about MySQL database:
+   if (at < 1 || dot < 1){
+      alert("Error in your email \n >>> '@' or '.' <<<");
+      return false;
+   }
+};
 
-$hostname = 'localhost';	## Hostname;
-$username = 'root';			## MySQL login;
-$password = '';				## MySQL password;
-$database = 'vinyl_bd';		## Database name;
-$usertable = "users";		## Table name;
+</script>
 
-####
-
-# Получение данных из формы
-$login = $_POST["login"];
-$password = md5($_POST["password"]);
- 
-# Получение данных сессии (если есть)
-session_start();
-$slogin = $_SESSION['login'];
-$spassword = $_SESSION['password'];
- 
-# Получение данных из базы
-$sql = mysqli_connect($hostname, $username, $password, $database) or die ("ERROR: MySQL.");
-$query = "SELECT login, password FROM administrators WHERE login='$login'";
-$queryRes = mysqli_query($sql, $query) or die(mysqli_error($sql));
-mysqli_close($sql);
-while($row = mysqli_fetch_assoc($queryRes)){
-    $blogin = $row['login'];
-    $bpassword = $row['password'];
-}
- 
-# Проверка если введенный логин соответствует логину из базы
-if ($login===$blogin) {
-    # Проверка если введенный пароль соответствует паролю из базы
-    if ($password===$bpassword) {
-        # Если все верно, создается сессия
-        session_start();
-        $_SESSION['login']=$login;
-        $_SESSION['password']=$password;
-        $logged = true;
-    }
-    else{
-        # Иначе не создается ничего
-        $logged = false;
-    }
-}
- 
-# Проверка если есть сессия
-if (is_null($slogin)) {
-    if (is_null($spassword)) {
-        if (!$logged) {
-        # Если сессии нет, клиент переадресуется на страницу авторизации
-        header("Location: http://".$_SERVER['SERVER_NAME']."/admin_panel/signin.php?signup=false");
-        }
-    }
-}
-
-?>
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </body>
+</html>
